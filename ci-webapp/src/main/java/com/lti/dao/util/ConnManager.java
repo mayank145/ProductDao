106d513c2c721ddabc9c597ab800172a5a414085
Mayank <choudharymayank145@gmail.com>
2020-08-08 16:22:30 +0530

    product data

---
+++ ci-webapp/src/main/java/com/lti/dao/util/ConnManager.java
@@ -0,0 +1,31 @@
+package com.lti.dao.util;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnManager {
+	
+	public static Connection connect() {
+		
+		try {
+			Properties dbProps=new Properties();
+			dbProps.load(ConnManager.class.getClassLoader().getResourceAsStream("dev-db.properties"));
+			
+			Class.forName(dbProps.getProperty("driverName"));
+			return DriverManager.getConnection(dbProps.getProperty("url"),dbProps.getProperty("user"), dbProps.getProperty("pass"));
+		}
+		
+		catch(ClassNotFoundException |IOException | SQLException e) {
+			e.printStackTrace();//not good should be thrown rather
+			return null;//very bad,should throw user defined exception
+		}
+		
+		
+		
+	}
+
+}
